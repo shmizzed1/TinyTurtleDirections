07a8cd1c867a341e9758f53b1040fb57bec459a1
Christian Cambizaca <christianismypapichulo@gmail.com>
2017-03-04 02:57:36 +0000

    tinyturtle

---
+++ script.js
@@ -0,0 +1,14 @@
+TinyTurtle.apply(window); 
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(45);
+forward(35);
+right(90);
+forward(35);
